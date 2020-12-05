2513bb6a0ce787323c0fb5d1d6d3e9271c5ff9a7
Alice Campkin <alice.campkin@gmail.com>
2020-12-05 20:33:52 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <layout />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Layout from "./components/Layout.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Layout,
+  },
+};
 </script>
 
 <style>
@@ -21,6 +20,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  height: 100vh;
+  width: 100vw;
 }
 </style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <body>
+    <header></header>
+    <main></main>
+    <footer></footer>
+  </body>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+body {
+  font-family: sans-serif;
+  background: pink;
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+</style>
